a2cfa3af6938a563b39053d18e9e058582405db4
Aria Hogan <70959736+sandersona2@users.noreply.github.com>
2024-03-27 12:17:45 -0400

    Update main.py

--- pygame_visualuml/main.py
+++ pygame_visualuml/main.py
@@ -78,35 +78,3 @@ while run:
     
     timer.tick(fps)
     pygame.display.update()
-
-"""
-for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                for num, box in enumerate(boxes):
-                    if box.rect.collidepoint(event.pos):
-                        active_box = num
-                    if box.text_box.collidepoint(event.pos):
-                        box.active = True
-                    else:
-                        box.active = False
-            
-
-        if event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:
-                active_box = None
-
-        if event.type == pygame.MOUSEMOTION:
-            if active_box != None:
-                boxes[active_box].rect.move_ip(event.rel)
-
-        if event.type == pygame.KEYDOWN:
-            print(event.unicode, event.unicode.isalpha())
-            for num, box in enumerate(boxes):
-                if event.unicode.isalpha() and box.active:
-                    box.user_text = box.user_text + event.unicode
-                if (event.key == pygame.K_BACKSPACE or event.key == pygame.K_DELETE) and box.active:
-                    box.user_text = box.user_text[:-1]
-                print(box.user_text)
-
-"""
